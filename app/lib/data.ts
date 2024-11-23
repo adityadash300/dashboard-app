041a9be3514b15404e8bad5aaaa421792b12306a
adityadash300 <adityadash300@gmail.com>
2024-11-23 15:04:20 +0530

    added pagination for invoices route

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -129,26 +129,26 @@ export async function fetchFilteredInvoices(
   }
 }
 
-// export async function fetchInvoicesPages(query: string) {
-//   try {
-//     const count = await sql`SELECT COUNT(*)
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE
-//       customers.name ILIKE ${`%${query}%`} OR
-//       customers.email ILIKE ${`%${query}%`} OR
-//       invoices.amount::text ILIKE ${`%${query}%`} OR
-//       invoices.date::text ILIKE ${`%${query}%`} OR
-//       invoices.status ILIKE ${`%${query}%`}
-//   `;
+export async function fetchInvoicesPages(query: string) {
+  try {
+    const count = await client.query(`SELECT COUNT(*)
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE
+      customers.name ILIKE '%' || $1 '%' OR
+      customers.email ILIKE '%' || $1 '%' OR
+      invoices.amount::text ILIKE '%' || $1 '%' OR
+      invoices.date::text ILIKE '%' || $1 '%' OR
+      invoices.status ILIKE '%' || $1 '%'
+  `, [query]);
 
-//     const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
-//     return totalPages;
-//   } catch (error) {
-//     console.error('Database Error:', error);
-//     throw new Error('Failed to fetch total number of invoices.');
-//   }
-// }
+    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    return totalPages;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch total number of invoices.');
+  }
+}
 
 // export async function fetchInvoiceById(id: string) {
 //   try {
