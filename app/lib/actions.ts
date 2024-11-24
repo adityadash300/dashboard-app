7b85a892f8bcba105da294bd2edb3670c49699bf
adityadash300 <adityadash300@gmail.com>
2024-11-24 11:13:10 +0530

    added server functions for validation and insert of invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,30 @@
 'use server';
 
-export async function createInvoice(formData:FormData) {
-    
+import { z } from 'zod';
+import { insertInvoice } from './data';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
+
+export async function createInvoice(formData: FormData) {
+    const rawFormData = Object.fromEntries(formData.entries())
+
+    const { customerId, amount, status } = CreateInvoice.parse(rawFormData)
+
+    const amountInCents = amount * 100
+    const date = new Date().toISOString().split('T')[0]
+
+    await insertInvoice({ customerId, amountInCents, status, date });
+
+    revalidatePath('/dashboard/invoice')
+    redirect('/dashboard/invoice')
 }
